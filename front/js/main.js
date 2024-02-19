e399875a1c926a3aeb758814bee326ae8c24487b
lordgr8t <lordgr8t@gmail.com>
2024-02-19 21:43:38 +0300

    main menu

---
+++ front/js/main.js
@@ -0,0 +1,18 @@
+
+
+
+$(document).ready(function() {
+	let item = $('.item').html();
+	$('.container').append('<div class="item">' + item + '</div>');
+	$('.container').append('<div class="item">' + item + '</div>');
+	$('.container').append('<div class="item">' + item + '</div>');
+	$('.container').append('<div class="item">' + item + '</div>');
+	$('.container').append('<div class="item">' + item + '</div>');
+	$('.container').append('<div class="item">' + item + '</div>');
+	$('.container').append('<div class="item">' + item + '</div>');
+	$('.container').append('<div class="item">' + item + '</div>');
+	$('.container').append('<div class="item">' + item + '</div>');
+});
+
+
+
